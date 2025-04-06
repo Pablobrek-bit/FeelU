1e6f1fd046a17d4e9692c39694e0049f292fe879
Pablo Henrique da Silva Andrade <pablobreak2017@alu.ufc.br>
2025-04-06 06:31:25 -0300

    feat: implement password hash verification

--- src/application/service/auth.service.ts
+++ src/application/service/auth.service.ts
@@ -2,24 +2,31 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from '../ports/user.repository';
 import { JwtService } from '@nestjs/jwt';
 import { LoginUserSchema } from '../dto/user/login-user-schema';
+import { compare } from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly userRepository: UserRepository,
-    private readonly jwtService: JwtService, // Ensure JwtService is injected
+    private readonly jwtService: JwtService,
   ) {}
 
-  async login(loginData: LoginUserSchema): Promise<{ accessToken: string }> {
+  async login(loginData: LoginUserSchema): Promise<{ token: string }> {
     const user = await this.userRepository.findUserByEmail(loginData.email);
 
-    if (!user || user.password !== loginData.password) {
+    if (!user) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+
+    const isPasswordValid = await compare(loginData.password, user.password);
+
+    if (!isPasswordValid) {
       throw new UnauthorizedException('Invalid email or password');
     }
 
     const payload = { sub: user.id, role: user.role };
-    const accessToken = this.jwtService.sign(payload);
+    const token = this.jwtService.sign(payload);
 
-    return { accessToken };
+    return { token };
   }
 }
