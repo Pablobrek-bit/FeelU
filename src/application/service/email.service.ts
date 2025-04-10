92a56897e2e13c37ebdf40ec3e9ffe6ff55a3052
Pablo Henrique da Silva Andrade <pablobreak2017@alu.ufc.br>
2025-04-09 21:18:57 -0300

    refactor: change the body of the email

--- src/application/service/email.service.ts
+++ src/application/service/email.service.ts
@@ -16,13 +16,23 @@ export class EmailService {
   async sendVerificationEmail(email: string, token: string): Promise<void> {
     const verificationUrl = `${process.env.APP_URL}/user/verify-email?token=${token}`;
 
+    console.log('token', token);
+
     try {
       await this.transporter.sendMail({
         from: process.env.EMAIL_FROM,
         to: email,
-        subject: 'Email Verification',
-        html: `<p>Click the link below to verify your email:</p>
-             <a href="${verificationUrl}">${verificationUrl}</a>`,
+        subject: 'Quase lá! só falta clicar nesse link 🔥',
+        html: `
+        <div style="font-family: Arial, sans-serif; text-align: center; color: #333;">
+          <h1>Bem-vindo ao Tindaria!</h1>
+          <p>Estamos quase lá! Clique no botão abaixo para verificar seu e-mail:</p>
+          <a href="${verificationUrl}" style="display: inline-block; padding: 10px 20px; color: #fff; background-color: #007bff; text-decoration: none; border-radius: 5px;">Verificar E-mail</a>
+          <p>Ou copie e cole o link no seu navegador:</p>
+          <p>${verificationUrl}</p>
+          <img src="cid:tindaria-logo" alt="Tindaria Logo" style="margin-top: 20px; width: 300px; height: auto;" />
+        </div>
+      `,
       });
     } catch (e) {
       console.error('Error sending email:', e);
