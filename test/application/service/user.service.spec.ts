5ff0395e9daedb3a3c119b0fb6d40bb27e2b36e2
Pablo Henrique da Silva Andrade <pablobreak2017@alu.ufc.br>
2025-04-16 20:05:09 -0300

    test: add test to create user method

--- test/application/service/user.service.spec.ts
+++ test/application/service/user.service.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 import { type TestingModule, Test } from '@nestjs/testing';
 import { UserRepository } from '../../../src/application/ports/user.repository';
 import { EmailService } from '../../../src/application/service/email.service';
@@ -14,27 +15,62 @@ import { createMockLikeService } from '../../mocks/like.service.mock';
 import { createMockProfileService } from '../../mocks/profile.service.mock';
 import { createMockRoleService } from '../../mocks/role.service.mock';
 import { createMockUserRepository } from '../../mocks/user.repository.mock';
+import type { CreateUserSchema } from '../../../src/application/dto/user/create-user-schema';
+import { UserAlreadyExistsError } from '../../../src/shared/exception/UserAlreadyExistsError';
 
 describe('UserService', () => {
   let service: UserService;
 
-  // Mock dependencies
-  //     private readonly userRepository: UserRepository,
   let mockUserRepository: ReturnType<typeof createMockUserRepository>;
-  //     private readonly filterService: FilterService,
   let mockFilterService: ReturnType<typeof createMockFilterService>;
-  //     private readonly profileService: ProfileService,
   let mockProfileService: ReturnType<typeof createMockProfileService>;
-  //     private readonly emailService: EmailService,
   let mockEmailService: ReturnType<typeof createMockEmailService>;
-  //     private readonly roleService: RoleService,
   let mockRoleService: ReturnType<typeof createMockRoleService>;
-  //     private readonly firebaseStorageService: FirebaseStorageService,
   let mockFirebaseStorageService: ReturnType<
     typeof createMockFirebaseStorageService
   >;
-  //     private readonly likeService: LikeService,
   let mockLikeService: ReturnType<typeof createMockLikeService>;
+
+  const userCreateData: CreateUserSchema = {
+    email: 'test@example.com',
+    password: 'password123',
+    filters: [
+      {
+        gender: 'MAN',
+        sexualOrientations: ['HETEROSEXUAL'],
+      },
+      {
+        gender: 'WOMAN',
+        sexualOrientations: ['HETEROSEXUAL'],
+      },
+    ],
+    profile: {
+      age: 25,
+      name: 'John Doe',
+      bio: 'Hello, world!',
+      course: 'Computer Science',
+      institution: 'University of Example',
+      emoji: '👨‍💻',
+      gender: 'MAN',
+      genderIsVisible: true,
+      sexualOrientation: 'HETEROSEXUAL',
+      sexualOrientationVisible: true,
+      instagramUrl: 'https://instagram.com/example',
+    },
+  };
+
+  const mockAvatar: Express.Multer.File = {
+    buffer: Buffer.from('mock-avatar-data'),
+    originalname: 'avatar.jpg',
+    destination: 'uploads/',
+    filename: 'avatar.jpg',
+    fieldname: 'avatar',
+    mimetype: 'image/jpg',
+    path: 'uploads/avatar.jpg',
+    size: 12345,
+    stream: new (require('stream').Readable)(),
+    encoding: '7bit',
+  };
 
   beforeEach(async () => {
     mockUserRepository = createMockUserRepository();
@@ -86,5 +122,58 @@ describe('UserService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('createUser', () => {
+    it('should create a user with a profile and filters', async () => {
+      // Arrange
+
+      const mockAvatarUrl = 'https://example.com/avatar.jpg';
+      (mockFirebaseStorageService.uploadFile as jest.Mock).mockResolvedValue(
+        mockAvatarUrl,
+      );
+
+      // Act
+      await service.createUser(userCreateData, mockAvatar);
+
+      // Assert
+      expect(mockUserRepository.existUserByEmail).toHaveBeenCalledWith(
+        userCreateData.email,
+      );
+      expect(mockUserRepository.createUser).toHaveBeenCalledTimes(1);
+      expect(mockFirebaseStorageService.uploadFile).toHaveBeenCalledWith(
+        mockAvatar,
+      );
+      expect(mockProfileService.createProfile).toHaveBeenCalledTimes(1);
+      expect(mockFilterService.createFilter).toHaveBeenCalledTimes(1);
+      expect(mockEmailService.sendVerificationEmail).toHaveBeenCalledWith(
+        userCreateData.email,
+        expect.any(String),
+      );
+    });
+
+    it('should throw an error if the user email already in use', async () => {
+      // Arrange
+      const existingEmail = 'existing@example.com';
+      (mockUserRepository.existUserByEmail as jest.Mock).mockResolvedValue(
+        true,
+      );
+      userCreateData.email = existingEmail;
+
+      // Act & Assert
+      await expect(
+        service.createUser(userCreateData, mockAvatar),
+      ).rejects.toThrow(UserAlreadyExistsError);
+      expect(mockUserRepository.existUserByEmail).toHaveBeenCalledWith(
+        existingEmail,
+      );
+      expect(mockUserRepository.createUser).not.toHaveBeenCalled();
+      expect(mockFirebaseStorageService.uploadFile).not.toHaveBeenCalled();
+      expect(mockProfileService.createProfile).not.toHaveBeenCalled();
+      expect(mockFilterService.createFilter).not.toHaveBeenCalled();
+      expect(mockEmailService.sendVerificationEmail).not.toHaveBeenCalled();
+      expect(mockUserRepository.createUser).not.toHaveBeenCalled();
+      expect(mockFirebaseStorageService.uploadFile).not.toHaveBeenCalled();
+    });
   });
 });
